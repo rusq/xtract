c7587ab661d697a5362e850e804df5a9ff9acaaa
Rustam Gilyazov <16064414+rusq@users.noreply.github.com>
2024-11-25 22:28:35 +1000

    add image and copyright string

--- xtract.c
+++ xtract.c
@@ -1,4 +1,5 @@
 /* Copyright (C) 2000  Timothy Hale and Mathieu Olivier
+ *           (C) 2024  rusq - porting to macOS and FreeBSD
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
